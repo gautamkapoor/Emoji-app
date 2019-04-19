742a579dae1481de01a4ca1ee8d0faffaed92285
Gautam Kapoor <legautam.17@gmail.com>
2019-04-19 15:42:26 +0530

    Changed emojis, added family

--- Emoji app/ViewController.swift
+++ Emoji app/ViewController.swift
@@ -18,27 +18,27 @@ class ViewController: UIViewController {
 //    }
     @IBAction func b1(_ sender: UIButton) {
         
-        let alertController = UIAlertController(title: "Welcome to my secnd app", message: "1", preferredStyle: UIAlertController.Style.alert)
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        let alertController = UIAlertController(title: "Mother", message: "I am Mousumi", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "Aight", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
     }
     
     
     @IBAction func b2(_ sender: UIButton) {
-        let alertController = UIAlertController(title: "Welcome to my seocnd app", message: "2", preferredStyle: UIAlertController.Style.alert)
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        let alertController = UIAlertController(title: "Father", message: "I am Navneet", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "Woooooow", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
     }
     
     @IBAction func b3(_ sender: UIButton) {
-        let alertController = UIAlertController(title: "Welcome to my seocnd app", message: "3", preferredStyle: UIAlertController.Style.alert)
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        let alertController = UIAlertController(title: "Son", message: "I am Gautam", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "Nice one", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
     }
     
     @IBAction func b4(_ sender: UIButton) {
-        let alertController = UIAlertController(title: "Welcome to my seocnd app", message: "4", preferredStyle: UIAlertController.Style.alert)
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        let alertController = UIAlertController(title: "Other son", message: "I am Sid", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "Cool", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
     }
 }
